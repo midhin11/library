d43da14e916039f4048ebe42397ac05f0ebdcef9
midhin11 <midhincr7@gmail.com>
2026-02-07 18:34:47 +0530

    Create form and button to get user data
    Created a button which cretaes a form on click and takes user data
    for books read and adds them to an array

--- js.js
+++ js.js
@@ -7,15 +7,66 @@ function Book(title, author, pages) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary() {
-
-}
-
+let title, author, pages;
 let addBookBtn = document.querySelector(".addbook-btn");
-let addBook = document.querySelector("addbook");
+let addBook = document.querySelector(".addbook");
 addBookBtn.addEventListener("click", ()=>{
-    
+    let form = document.createElement("form");
+    form.setAttribute("action", ".");
+    form.setAttribute("method", "post");
+
+    let titleInputDiv = document.createElement("div");
+    let titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("id", "title");
+    titleInput.setAttribute("name", "title");
+    titleInput.setAttribute("value", "title");
+    let titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "title");
+    titleLabel.textContent = "Title";
+    titleInputDiv.append(titleLabel, titleInput );
+    form.appendChild(titleInputDiv);
+
+    let authorInputDiv = document.createElement("div");
+    let authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("id", "author");
+    authorInput.setAttribute("name", "author");
+    authorInput.setAttribute("value", "author");
+    let authorLabel = document.createElement("label");
+    authorLabel.setAttribute("for", "author");
+    authorLabel.textContent = "Author";
+    authorInputDiv.append(authorLabel, authorInput);
+    form.appendChild(authorInputDiv);
+
+    let pagesInputDiv = document.createElement("div");
+    let pagesInput = document.createElement("input");
+    pagesInput.setAttribute("type", "number");
+    pagesInput.setAttribute("id", "pages");
+    pagesInput.setAttribute("name", "pages");
+    pagesInput.setAttribute("value", "20");
+    let pagesLabel = document.createElement("label");
+    pagesLabel.setAttribute("for", "pages");
+    pagesLabel.textContent = "Pages";
+    pagesInputDiv.append(pagesLabel, pagesInput);
+    form.appendChild(pagesInputDiv);
+
+    let submitValueBtn = document.createElement("button");
+    submitValueBtn.textContent = "Submit";
+    form.appendChild(submitValueBtn);
+    submitValueBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        title = titleInput.value;
+        author = authorInput.value;
+        pages = pagesInput.value;
+
+        myLibrary.push(new Book(title, author, pages));
+        addBook.removeChild(form);
+    })
+
+    addBook.appendChild(form);
 })
+
 
 let exBook = new Book("Title", "author", 244);
 console.log(exBook.title);
