7cd4e04008bb0423de062c61473d70dc532f2527
midhin11 <midhincr7@gmail.com>
2026-04-15 21:38:20 +0530

    Add constraint validation to input fields

--- js.js
+++ js.js
@@ -10,105 +10,82 @@ class Book {
 }
 
 let title, author, pages;
+let titleInput = document.querySelector("#title");
+let authorInput = document.querySelector("#author");
+let pagesInput = document.querySelector("#pages");
 let addBookBtn = document.querySelector(".addbook-btn");
 let addBook = document.querySelector(".addbook");
+let form = document.querySelector("form");
+let cards = document.querySelector(".cards");
 addBookBtn.addEventListener("click", ()=>{
     addBookBtn.disabled = true;
-    let form = document.createElement("form");
-    form.setAttribute("method", "post");
-
-    let titleInputDiv = document.createElement("div");
-    let titleInput = document.createElement("input");
-    titleInput.setAttribute("type", "text");
-    titleInput.setAttribute("id", "title");
-    titleInput.setAttribute("name", "title");
-    titleInput.setAttribute("placeholder", "The name of the book");
-    titleInput.required = true;
-    let titleLabel = document.createElement("label");
-    titleLabel.setAttribute("for", "title");
-    titleLabel.textContent = "Title";
-    titleInputDiv.append(titleLabel, titleInput );
-    form.appendChild(titleInputDiv);
-
-    let authorInputDiv = document.createElement("div");
-    let authorInput = document.createElement("input");
-    authorInput.setAttribute("type", "text");
-    authorInput.setAttribute("id", "author");
-    authorInput.setAttribute("name", "author");
-    authorInput.setAttribute("placeholder", "Who was it written by?");
-    authorInput.required = true;
-    let authorLabel = document.createElement("label");
-    authorLabel.setAttribute("for", "author");
-    authorLabel.textContent = "Author";
-    authorInputDiv.append(authorLabel, authorInput);
-    form.appendChild(authorInputDiv);
-
-    let pagesInputDiv = document.createElement("div");
-    let pagesInput = document.createElement("input");
-    pagesInput.setAttribute("type", "number");
-    pagesInput.setAttribute("id", "pages");
-    pagesInput.setAttribute("name", "pages");
-    pagesInput.required = true;
-    pagesInput.setAttribute("placeholder", "How many pages where there?");
-    let pagesLabel = document.createElement("label");
-    pagesLabel.setAttribute("for", "pages");
-    pagesLabel.textContent = "Pages";
-    pagesInputDiv.append(pagesLabel, pagesInput);
-    form.appendChild(pagesInputDiv);
-
-    let submitValueBtn = document.createElement("button");
-    submitValueBtn.type = "submit";
-    submitValueBtn.textContent = "Submit";
-    submitValueBtn.setAttribute("class", "submit-data")
-    form.appendChild(submitValueBtn);
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        addBookBtn.disabled = false;
-        title = titleInput.value.trim();
-        author = authorInput.value.trim();
-        pages = pagesInput.value.trim();
-
-        myLibrary.push(new Book(title, author, pages));
-        addBook.removeChild(form);
-
-        let cards = document.querySelector(".cards");
-
-        let newBookTitleContainer = document.createElement("div");
-        newBookTitleContainer.setAttribute("class", "title");
-        let newBookTitleHeader = document.createElement("div");
-        newBookTitleHeader.textContent = "Title:";
-        let newBookTitle = document.createElement("div");
-        newBookTitle.textContent = myLibrary[0].title;
-        newBookTitleContainer.append(newBookTitleHeader, newBookTitle);
-
-        let newBookAuthorContainer =  document.createElement("div");
-        newBookAuthorContainer.setAttribute("class", "author");
-        let newBookAuthorHeader = document.createElement("div");
-        newBookAuthorHeader.textContent = "Written by:";
-        let newBookAuthor = document.createElement("div");
-        newBookAuthor.textContent = myLibrary[0].author;
-        newBookAuthorContainer.append(newBookAuthorHeader, newBookAuthor);
-
-        let newBookPagesContainer =  document.createElement("div");
-        newBookPagesContainer.setAttribute("class", "pages");
-        let newBookPagesHeader = document.createElement("div");
-        newBookPagesHeader.textContent = "Pages:";
-        let newBookPages = document.createElement("div");
-        newBookPages.textContent = myLibrary[0].pages;
-        newBookPagesContainer.append(newBookPagesHeader, newBookPages);
-
-        let card = document.createElement("div");
-        card.setAttribute("class", "card");
-        card.append(newBookTitleContainer, newBookAuthorContainer, newBookPagesContainer)
-
-        cards.append(card);
-        myLibrary.pop();
-    })
-
-    addBook.appendChild(form);
+    form.classList.remove("hidden");
 })
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateForm();
 
-let exBook = new Book("Title", "author", 244);
-console.log(exBook.title);
-console.log(exBook.id);
+    addBookBtn.disabled = false;
+    title = titleInput.value.trim();
+    author = authorInput.value.trim();
+    pages = pagesInput.value.trim();
+
+    let book = new Book(title, author, pages)
+    myLibrary.push(book);
+    form.classList.add("hidden");
+
+    let newBookTitleContainer = document.createElement("div");
+    newBookTitleContainer.setAttribute("class", "title");
+    let newBookTitleHeader = document.createElement("div");
+    newBookTitleHeader.textContent = "Title:";
+    let newBookTitle = document.createElement("div");
+    newBookTitle.textContent = book.title;
+    newBookTitleContainer.append(newBookTitleHeader, newBookTitle);
+
+    let newBookAuthorContainer =  document.createElement("div");
+    newBookAuthorContainer.setAttribute("class", "author");
+    let newBookAuthorHeader = document.createElement("div");
+    newBookAuthorHeader.textContent = "Written by:";
+    let newBookAuthor = document.createElement("div");
+    newBookAuthor.textContent = book.author;
+    newBookAuthorContainer.append(newBookAuthorHeader, newBookAuthor);
+
+    let newBookPagesContainer =  document.createElement("div");
+    newBookPagesContainer.setAttribute("class", "pages");
+    let newBookPagesHeader = document.createElement("div");
+    newBookPagesHeader.textContent = "Pages:";
+    let newBookPages = document.createElement("div");
+    newBookPages.textContent = book.pages;
+    newBookPagesContainer.append(newBookPagesHeader, newBookPages);
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.append(newBookTitleContainer, newBookAuthorContainer, newBookPagesContainer)
+
+    cards.append(card);
+})
+
+titleInput.addEventListener("input", validateForm);
+authorInput.addEventListener("input", validateForm);
+pagesInput.addEventListener("input", validateForm);
+
+function validateForm() {
+    if (titleInput.validity.valueMissing) {
+        titleInput.setCustomValidity("Title cannot be empty");
+    } else {
+        titleInput.setCustomValidity("");
+    }
+
+    if (authorInput.validity.valueMissing) {
+        authorInput.setCustomValidity("Author cannot be empty");
+    } else {
+        authorInput.setCustomValidity("");
+    }
+
+    if (pagesInput.validity.valueMissing) {
+        pagesInput.setCustomValidity("Pages required");
+    } else {
+        pagesInput.setCustomValidity("");
+    }
+}
